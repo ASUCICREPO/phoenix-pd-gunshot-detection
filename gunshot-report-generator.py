a3506247210c5550eff70aeed454a05d79f76a10
risabhrizz <rraj21@asu.edu>
2022-07-05 17:08:47 -0700

    Update gunshot report generator script

--- gunshot-report-generator.py
+++ gunshot-report-generator.py
@@ -1,5 +1,6 @@
 import boto3
 import pandas as pd
+import os.path
 
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
@@ -21,7 +22,11 @@ df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms').dt.tz_localize(None
 
 
 df = df.sort_values(by='timestamp', ascending=True)
+
+df = df.rename(columns={'timestamp': 'Timestamp', 's3_url': 'Captured Sound URL', 'device_id': 'Device ID'})
+df['Classification'] = pd.Series(dtype='object')
 print(df.head())
 
 # Create Excel sheet
-# df.to_excel("Gunshot Deployment Report.xlsx")
+if not os.path.isfile('Gunshot Deployment Report.xlsx'):
+    df.to_excel("Gunshot Deployment Report.xlsx", index=False)
